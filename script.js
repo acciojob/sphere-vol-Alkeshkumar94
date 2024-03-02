ba33060d726390ec82aa3d134808abcda9d5609c
Alkesh kumar <127075190+Alkeshkumar94@users.noreply.github.com>
2024-03-02 08:37:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function volume_sphere() {
     //Write your code here
-  
+	  event.preventDefault();
+
+    // Get the radius input value
+    var radius = document.getElementById('radius').value;
+
+    // Calculate the volume of the sphere
+    var volume = (4 / 3) * Math.PI * Math.pow(radius, 3);
+
+    // Update the volume input value
+    document.getElementById('volume').value = volume.toFixed(4);
 } 
 
 window.onload = document.getElementById('MyForm').onsubmit = volume_sphere;
